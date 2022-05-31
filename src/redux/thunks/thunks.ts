ecd099ac637b052558084d61b103a52ec567d2ff
Elías Heresi <elias.heresi@gmail.com>
2022-05-31 09:30:23 +0200

    First register thunk version

--- src/redux/thunks/thunks.ts
+++ src/redux/thunks/thunks.ts
@@ -1,5 +1,4 @@
-import { Action, ThunkDispatch } from "@reduxjs/toolkit";
-import { Statement } from "typescript";
+import axios from "axios";
 
 interface IRegister {
   name: string;
@@ -9,30 +8,7 @@ interface IRegister {
   password: string;
 }
 
-export const registerUserThunk =
-  (formData: IRegister) =>
-  async (dispatch: ThunkDispatch<Statement, void, Action>) => {};
-
-// export const registerUserThunk = (formData) => async (dispatch) => {
-//   try {
-//     dispatch(loadingActionCreator());
-//     await axios.post(`${process.env.REACT_APP_API_URL}user/register`, formData);
-
-//     const newUser = {
-//       username: formData.username,
-//       password: formData.password,
-//     };
-
-//     const route = `${process.env.REACT_APP_API_URL}user/login`;
-//     const {
-//       data: { token },
-//     } = await axios.post(route, newUser);
-//     localStorage.setItem("token", token);
-//     const userInfo = jwtDecode(token);
-
-//     dispatch(loginActionCreator(userInfo));
-//     dispatch(finishedLoadingActionCreator());
-//   } catch (error) {
-//     dispatch(finishedLoadingActionCreator());
-//   }
-// };
+export const registerUserThunk = (formData: IRegister) => async () => {
+  let route: string = `${process.env.REACT_APP_API_URL}user/register`;
+  await axios.post(route, formData);
+};
