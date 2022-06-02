750947c5a0dabe31fb4288e5531d7413e14fda25
Elías Heresi <elias.heresi@gmail.com>
2022-06-02 13:58:23 +0200

    Updated login and register tests

--- src/pages/Login/Login.test.tsx
+++ src/pages/Login/Login.test.tsx
@@ -2,13 +2,16 @@ import Login from "./Login";
 import TestRenderer from "react-test-renderer";
 import { Provider } from "react-redux";
 import store from "../../redux/store/store";
+import { BrowserRouter } from "react-router-dom";
 
 describe("Given the Login page component", () => {
   describe("When it's invoked", () => {
     test("Then it should always match this snapshot", () => {
       const testedLoginPage = TestRenderer.create(
         <Provider store={store}>
-          <Login />
+          <BrowserRouter>
+            <Login />
+          </BrowserRouter>
         </Provider>
       );
 
--- src/pages/Register/Register.test.tsx
+++ src/pages/Register/Register.test.tsx
@@ -2,13 +2,16 @@ import TestRenderer from "react-test-renderer";
 import { Provider } from "react-redux";
 import store from "../../redux/store/store";
 import Register from "./Register";
+import { BrowserRouter } from "react-router-dom";
 
 describe("Given the Register page component", () => {
   describe("When it's invoked", () => {
     test("Then it should always match this snapshot", () => {
       const testedRegisterPage = TestRenderer.create(
         <Provider store={store}>
-          <Register />
+          <BrowserRouter>
+            <Register />
+          </BrowserRouter>
         </Provider>
       );
 
