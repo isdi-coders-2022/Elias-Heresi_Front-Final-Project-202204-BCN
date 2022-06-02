fb25d51689e67d245e5a28f892a6102ea92e86fa
Elías Heresi <elias.heresi@gmail.com>
2022-06-02 16:42:06 +0200

    Deleted screen.debug from EntrySummary test

--- src/components/EntrySummary/EntrySummary.test.tsx
+++ src/components/EntrySummary/EntrySummary.test.tsx
@@ -6,7 +6,6 @@ describe("Given the EntrySummary component", () => {
   describe("When invoked", () => {
     test("Then a canvas element and a button will be renderized", () => {
       render(<EntrySummary entry={mockApiGetResponse[0]} />);
-      screen.debug();
 
       const searchedCanvas = screen.getByRole("img");
       const searchedButton = screen.getByRole("button");
