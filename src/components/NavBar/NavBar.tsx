5854bd54e64cabdf31ae8ce33355d57ad886dde1
Elías Heresi <elias.heresi@gmail.com>
2022-06-03 08:52:14 +0200

    Added navigation and actions to navBar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,19 +1,24 @@
 import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
 import { FaUser } from "react-icons/fa";
+import { useAppDispatch } from "../../redux/store/hooks";
+import { logOutUserThunk } from "../../redux/thunks/userThunks/userThunks";
 
 const NavBar = (): JSX.Element => {
-  //"User" + {};
-  //
+  const dispatch = useAppDispatch();
+
+  const logout = () => {
+    dispatch(logOutUserThunk());
+  };
 
   return (
     <Navbar bg="light" expand="lg">
       <Container>
         <Navbar.Brand href="#home">Bonanza</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle aria-controls="/historic" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Past entries</Nav.Link>
+            <Nav.Link href="/historic">Home</Nav.Link>
+            <Nav.Link href="/historic">Past entries</Nav.Link>
             <Nav.Link href="#link">Know thyself</Nav.Link>
             <NavDropdown
               title={
@@ -26,7 +31,7 @@ const NavBar = (): JSX.Element => {
             >
               <NavDropdown.Item href="#action/3.1">My profile</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Logout</NavDropdown.Item>
+              <NavDropdown.Item onClick={logout}>Logout</NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
