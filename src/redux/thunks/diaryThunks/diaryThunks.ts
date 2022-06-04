3fea73275f34cff342c36a62a6a4afb284a64ef5
Elías Heresi <elias.heresi@gmail.com>
2022-06-04 17:50:52 +0200

    Created deleteEntry thunk and in slice

--- src/redux/thunks/diaryThunks/diaryThunks.ts
+++ src/redux/thunks/diaryThunks/diaryThunks.ts
@@ -1,10 +1,18 @@
 import axios from "axios";
 import { AppDispatch } from "../../store/store";
 import { GetApiResponse } from "../../interfaces/DiaryInterface";
-import { loadActionCreator } from "../../features/diarySlice";
+import {
+  deleteEntryActionCreator,
+  loadActionCreator,
+} from "../../features/diarySlice";
+import {
+  finishedLoadingActionCreator,
+  loadingActionCreator,
+} from "../../features/uiSlice";
 
 export const loadEntriesThunk =
   (key: string) => async (dispatch: AppDispatch) => {
+    dispatch(loadingActionCreator());
     const diaryRoute: string = `${process.env.REACT_APP_API_URL}diary/all`;
     const token = `Bearer ${key}`;
     const {
@@ -13,4 +21,18 @@ export const loadEntriesThunk =
       headers: { Authorization: token },
     });
     dispatch(loadActionCreator(entries));
+    dispatch(finishedLoadingActionCreator());
+  };
+
+export const deleteEntryThunk =
+  (key: string, entryId: string) => async (dispatch: AppDispatch) => {
+    dispatch(loadingActionCreator());
+    const diaryRoute: string = `${process.env.REACT_APP_API_URL}diary/delete`;
+    const token = `Bearer ${key}`;
+    await axios.delete(diaryRoute, {
+      headers: { Authorization: token },
+      data: { entryId },
+    });
+    dispatch(deleteEntryActionCreator(entryId));
+    dispatch(finishedLoadingActionCreator());
   };
