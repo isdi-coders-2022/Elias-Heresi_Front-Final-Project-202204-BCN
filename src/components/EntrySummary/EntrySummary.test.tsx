f5e2278c367b5852a3304bc9c6fbb0c3f48bc758
Elías Heresi <elias.heresi@gmail.com>
2022-06-05 12:43:51 +0200

    Tested dispatch calls in confirmation modal

--- src/components/EntrySummary/EntrySummary.test.tsx
+++ src/components/EntrySummary/EntrySummary.test.tsx
@@ -1,12 +1,16 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { Provider } from "react-redux";
+import { deleteEntryActionCreator } from "../../redux/features/diarySlice";
 import {
+  feedbackOffActionCreator,
   feedbackOnActionCreator,
+  resetEntryIdActionCreator,
   saveEntryIdActionCreator,
 } from "../../redux/features/uiSlice";
 import { mockApiGetResponse, mockApiId } from "../../redux/mocks/diaryMocks";
 import store from "../../redux/store/store";
+import { deleteEntryThunk } from "../../redux/thunks/diaryThunks/diaryThunks";
 import EntrySummary from "./EntrySummary";
 
 jest.mock("react-chartjs-2", () => ({
@@ -14,10 +18,12 @@ jest.mock("react-chartjs-2", () => ({
 }));
 
 const mockDispatch = jest.fn();
+let mockSelector = false;
 
 jest.mock("react-redux", () => ({
   ...jest.requireActual("react-redux"),
   useDispatch: () => mockDispatch,
+  useSelector: () => ({ feedback: mockSelector }),
 }));
 
 describe("Given the EntrySummary component", () => {
@@ -37,6 +43,7 @@ describe("Given the EntrySummary component", () => {
       expect(searchedButton).toHaveLength(expectedButtons);
     });
   });
+
   describe("When the top right corner is clicked", () => {
     test("Then the dispatch will be called with the feedbackOn and saveEntry actions", () => {
       render(
@@ -45,14 +52,35 @@ describe("Given the EntrySummary component", () => {
         </Provider>
       );
 
-      const deleteCardButton = screen.getAllByRole("button");
-
-      userEvent.click(deleteCardButton[0]);
+      const deleteCardButton = screen.getAllByRole("button")[0];
+      userEvent.click(deleteCardButton);
 
       expect(mockDispatch).toHaveBeenCalledWith(feedbackOnActionCreator());
       expect(mockDispatch).toHaveBeenCalledWith(
         saveEntryIdActionCreator(mockApiId)
       );
+    });
+  });
+  describe("When the top right corner and the confirmation button", () => {
+    test("Then dispatch will have been called 5 times, now also with the resetEntryId and the feedbackOff actions", () => {
+      mockSelector = true;
+      const expectedNumberOfCalls = 5;
+
+      render(
+        <Provider store={store}>
+          <EntrySummary entry={mockApiGetResponse[0]} />
+        </Provider>
+      );
+
+      const deleteCardButton = screen.getAllByRole("button")[0];
+      userEvent.click(deleteCardButton);
+
+      const confirmButton = screen.getByRole("button", { name: "Yes" });
+      userEvent.click(confirmButton);
+
+      expect(mockDispatch).toHaveBeenCalledTimes(expectedNumberOfCalls);
+      expect(mockDispatch).toHaveBeenCalledWith(resetEntryIdActionCreator());
+      expect(mockDispatch).toHaveBeenCalledWith(feedbackOffActionCreator());
     });
   });
 });
