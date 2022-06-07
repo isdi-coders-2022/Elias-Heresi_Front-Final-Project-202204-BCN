4a168057b781e5b8f2742ffa6c6e4537a455259a
Elías Heresi <elias.heresi@gmail.com>
2022-06-07 10:39:00 +0200

    Replaced href with onClick in navBar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,27 +1,38 @@
 import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
 import { FaUser } from "react-icons/fa";
+import { Link, useNavigate } from "react-router-dom";
 import { useAppDispatch } from "../../redux/store/hooks";
 import { logOutUserThunk } from "../../redux/thunks/userThunks/userThunks";
 import NavBarContainer from "./NavBarContainer";
 
 const NavBar = (): JSX.Element => {
   const dispatch = useAppDispatch();
+  const navigate = useNavigate();
 
   const logout = () => {
     dispatch(logOutUserThunk());
+  };
+
+  const navigateToPage = (page: string): void => {
+    navigate(`/${page}`);
   };
 
   return (
     <NavBarContainer>
       <Navbar expand="lg">
         <Container>
-          <Navbar.Brand href="/historic" className="ml-auto">
+          <Navbar.Brand
+            onClick={() => navigateToPage("historic")}
+            className="ml-auto"
+          >
             Bonanza
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav" className="ml-auto">
-            <Nav.Link href="/historic">Historic</Nav.Link>
-            <Nav.Link href="/create">Create</Nav.Link>
+            <Nav.Link onClick={() => navigateToPage("historic")}>
+              Historic
+            </Nav.Link>
+            <Nav.Link onClick={() => navigateToPage("create")}>Create</Nav.Link>
           </Navbar.Collapse>
           <NavDropdown
             title={
@@ -32,7 +43,8 @@ const NavBar = (): JSX.Element => {
             }
             id="basic-nav-dropdown"
           >
-            <NavDropdown.Item href="/historic">My profile</NavDropdown.Item>
+            <NavDropdown.Item>My profile</NavDropdown.Item>
+
             <NavDropdown.Divider />
             <NavDropdown.Item onClick={logout}>Logout</NavDropdown.Item>
           </NavDropdown>
