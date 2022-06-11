0d02b8dcd62e9ba09562108e8f61a6bde6c93598
Elías Heresi <elias.heresi@gmail.com>
2022-06-11 22:18:57 +0200

    Added the dispatching of the filteredEntryThunkd to the date filter

--- src/components/DateFilter/DateFilter.tsx
+++ src/components/DateFilter/DateFilter.tsx
@@ -1,15 +1,17 @@
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 import { Button, Form, Row, Col } from "react-bootstrap";
 import { FilterDates } from "../../redux/interfaces/DiaryInterface";
+import { useAppDispatch } from "../../redux/store/hooks";
+import { loadFilteredEntriesThunk } from "../../redux/thunks/diaryThunks/diaryThunks";
 import { dateToNumber } from "../../utils/todaysDate";
 import { DateFilterContainer } from "./DateFilterContainer";
 
 const DateFilter = (): JSX.Element => {
-  const todaysDate = dateToNumber(new Date());
+  const dispatch = useAppDispatch();
 
   const formInitialState = {
-    startDate: todaysDate,
-    endDate: todaysDate,
+    startDate: dateToNumber(new Date(2100, 1, 1)),
+    endDate: dateToNumber(new Date(1900, 1, 1)),
   } as FilterDates;
 
   const [formData, setFormData] = useState(formInitialState);
@@ -22,21 +24,33 @@ const DateFilter = (): JSX.Element => {
     });
   };
 
+  const resetDates = (): void => {
+    setFormData(formInitialState);
+  };
+
+  const filterEntries = (event: FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
+    dispatch(loadFilteredEntriesThunk(formData));
+    resetDates();
+  };
+
   return (
     <DateFilterContainer>
-      <Row>
-        <Col>
-          <Form.Control type="date" id="startDate" onChange={changeData} />
-        </Col>
-        <Col>
-          <Form.Control type="date" id="endDate" onChange={changeData} />
-        </Col>
-        <Col>
-          <Button variant="primary" type="submit">
-            Filter
-          </Button>
-        </Col>
-      </Row>
+      <Form autoComplete="off" onSubmit={filterEntries} noValidate>
+        <Row>
+          <Col>
+            <Form.Control type="date" id="startDate" onChange={changeData} />
+          </Col>
+          <Col>
+            <Form.Control type="date" id="endDate" onChange={changeData} />
+          </Col>
+          <Col>
+            <Button variant="primary" type="submit">
+              Filter
+            </Button>
+          </Col>
+        </Row>
+      </Form>
     </DateFilterContainer>
   );
 };
