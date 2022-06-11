430f9d9486fee8fb5fd11c936437a6446fac4d92
Elías Heresi <elias.heresi@gmail.com>
2022-06-11 21:58:55 +0200

    Added state containing dates in DateFilter

--- src/components/DateFilter/DateFilter.tsx
+++ src/components/DateFilter/DateFilter.tsx
@@ -1,15 +1,35 @@
+import { ChangeEvent, useState } from "react";
 import { Button, Form, Row, Col } from "react-bootstrap";
+import { FilterDates } from "../../redux/interfaces/DiaryInterface";
+import { dateToNumber } from "../../utils/todaysDate";
 import { DateFilterContainer } from "./DateFilterContainer";
 
 const DateFilter = (): JSX.Element => {
+  const todaysDate = dateToNumber(new Date());
+
+  const formInitialState = {
+    startDate: todaysDate,
+    endDate: todaysDate,
+  } as FilterDates;
+
+  const [formData, setFormData] = useState(formInitialState);
+
+  const changeData = (event: ChangeEvent<HTMLInputElement>): void => {
+    event.preventDefault();
+    setFormData({
+      ...formData,
+      [event.target.id]: +event.target.value.replaceAll("-", ""),
+    });
+  };
+
   return (
     <DateFilterContainer>
       <Row>
         <Col>
-          <Form.Control type="date" name="startDate" />
+          <Form.Control type="date" id="startDate" onChange={changeData} />
         </Col>
         <Col>
-          <Form.Control type="date" name="endDate" />
+          <Form.Control type="date" id="endDate" onChange={changeData} />
         </Col>
         <Col>
           <Button variant="primary" type="submit">
