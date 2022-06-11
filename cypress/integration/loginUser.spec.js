27cacf9aede704fe0fab6c87e51b54a392495be0
Elías Heresi <elias.heresi@gmail.com>
2022-06-11 19:52:03 +0200

    Created Cypress login test

---
+++ cypress/integration/loginUser.spec.js
@@ -0,0 +1,10 @@
+describe("Given the Bonanza application and its login page", () => {
+  describe("When the user 'test' logs in with her password", () => {
+    it("Then she will be redirected to the historic page", () => {
+      cy.visit("login");
+      cy.get("label").contains("Username").type("test");
+      cy.get("label").contains("Password").type(`1234{enter}`);
+      cy.url().should("include", "/historic");
+    });
+  });
+});
