f24f68fe5aed1783b97273be22988288dbeb070d
Elías Heresi <elias.heresi@gmail.com>
2022-06-14 15:45:46 +0200

    Corrected test in Not Found

--- src/pages/NotFound/NotFound.test.tsx
+++ src/pages/NotFound/NotFound.test.tsx
@@ -2,22 +2,7 @@ import { Provider } from "react-redux";
 import store from "../../redux/store/store";
 import { BrowserRouter } from "react-router-dom";
 import { render, screen } from "@testing-library/react";
-import Login from "./Login";
 import NotFound from "./NotFound";
-
-const mockUseNavigate = jest.fn();
-
-jest.mock("react-router-dom", () => ({
-  ...jest.requireActual("react-router-dom"),
-  useNavigate: () => mockUseNavigate,
-}));
-
-let mockLogged: boolean = false;
-
-jest.mock("react-redux", () => ({
-  ...jest.requireActual("react-redux"),
-  useSelector: () => ({ logged: mockLogged }),
-}));
 
 describe("Given the Not Found page component", () => {
   describe("When it's invoked", () => {
@@ -31,7 +16,7 @@ describe("Given the Not Found page component", () => {
       );
 
       const searchedHeaders = screen.getAllByRole("heading");
-      const searchedImages = 1;
+      const searchedImages = screen.getAllByRole("img");
 
       const expectedHeaders = 1;
       const expectedImages = 1;
